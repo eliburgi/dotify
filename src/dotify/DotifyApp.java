406eaa7fdfd6f188c340af5e0061c7299e03e069
7Magic7Mike7 <michael.artner@hotmail.de>
2018-04-26 11:18:11 +0200

    test commit

--- src/dotify/DotifyApp.java
+++ src/dotify/DotifyApp.java
@@ -13,7 +13,7 @@ public class DotifyApp extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("play/play_screen.fxml"));
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("Dotify");
-        primaryStage.setMinWidth(1360);
+        primaryStage.setMinWidth(360);      //change from 1360 to 360
         primaryStage.setMinHeight(768);
         primaryStage.show();
     }
